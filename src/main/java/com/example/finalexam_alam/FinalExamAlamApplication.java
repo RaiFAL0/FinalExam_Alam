057cc71c174c4ed94f3ce604bff739face453bb1
Rafin Ifta Alam <wi22b015@technikum-wien.at>
2023-06-11 20:55:00 +0200

    first commit

---
+++ src/main/java/com/example/finalexam_alam/FinalExamAlamApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalexam_alam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalExamAlamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalExamAlamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/finalexam_alam/FinalExamAlamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalexam_alam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalExamAlamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
